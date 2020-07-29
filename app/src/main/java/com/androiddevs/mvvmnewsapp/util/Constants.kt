94d8b04a8ba20d36ea810ec7257854c88250d288
behrooz pahlavan <pahlavan111@gmail.com>
2020-07-29 08:12:18 +0430

    add RetrofitInstance class to api package

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -3,5 +3,6 @@ package com.androiddevs.mvvmnewsapp.util
 class Constants {
     companion object{
         const val API_KEY = "174555fc55f94e4d85f315746e6950a5"
+        const val BASE_URL = "https://newsapi.org/"
     }
 }
